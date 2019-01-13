1b52ed2d4449508d0bb1caf0f6ed232db966437a
Alkaid <zgf574564920@gmail.com>
2019-01-13 00:24:33 -0500

    make corruption_test multithread
    each thread operates different file.
    randomly sleep at the very beginning, to create overlapping read and write
    the number of threads and sleep duration are hardcoded

--- libfs/tests/corruption_test.c
+++ libfs/tests/corruption_test.c
@@ -1,4 +1,7 @@
+#define _GNU_SOURCE
 #include <sys/stat.h>
+#include <sys/syscall.h>
+#include <pthread.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -7,11 +10,11 @@
 #include <unistd.h>
 #include <mlfs/mlfs_interface.h>
 #define BLOCK_SIZE 81920
-#define MAX_REPLAY 8192000
-int fd;
-int replay_log[MAX_REPLAY];
+#define THREADS 5
+int *replay_log = NULL;
 int replay_size = 0;
-int total_size;
+int total_size = 0;
+void *worker_thread(void*);
 int main(int argc, char **argv) {
     if (argc < 3) {
         fprintf(stderr, "usage %s file_path r/w/rw\n", argv[0]);
@@ -20,52 +23,64 @@ int main(int argc, char **argv) {
     printf("waiting\n");
     fflush(stdout);
     sleep(3);
+
+    scanf("%d", &replay_size);
+    replay_log = malloc(sizeof(int)*replay_size);
+    int size;
+    int ri=0;
+    while (scanf("%d", &size) != EOF) {
+        replay_log[ri++] = size;
+        if (size > BLOCK_SIZE) {
+            printf("size %d too large\n", size);
+            exit(-1);
+        }
+        total_size += size;
+    }
+    pthread_t threads[THREADS];
+    for (int i=0; i < THREADS; ++i) {
+        assert(pthread_create(&threads[i], NULL, worker_thread, (void*)argv) == 0);
+    }
+    for (int i=0; i < THREADS; ++i) {
+        assert(pthread_join(threads[i], NULL) == 0);
+    }
+    return 0;
+}
+void *worker_thread(void *arg) {
+    int fd;
+    char **argv = (char**)arg;
+    char fn[1024];
+    sleep(rand()%10);
+    sprintf(fn, "%s.%u", argv[1], pthread_self());
     if ((strcmp(argv[2], "w") == 0) || (strcmp(argv[2], "rw") == 0)) {
-        fd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0666);
+        fd = open(fn, O_CREAT | O_TRUNC | O_RDWR, 0666);
         if (fd == -1) {
             perror("open failed");
             exit(-1);
         }
         unsigned char *write_buf = malloc(BLOCK_SIZE);
-        unsigned int size;
         unsigned int offset = 0;
         printf("running\n");
-        while (scanf("%d", &size) != EOF) {
-            replay_log[replay_size++] = size;
-            if (size > BLOCK_SIZE) {
-                printf("size %d too large\n", size);
-                exit(-1);
-            }
+        for (int ri=0; ri < replay_size; ++ri) {
+            unsigned int size = replay_log[ri];
             for (unsigned int i=0; i < size; ++i) {
                 write_buf[i] = (offset + i)%(256U);
             }
             write(fd, write_buf, size);
             offset += size;
         }
+        assert(offset == total_size);
         struct stat file_stat;
         fstat(fd, &file_stat);
         close(fd);
         fsync(fd);
-        total_size = offset;
-        printf("write done, to fd %d, total %d, st_size %d\n", fd, total_size, file_stat.st_size);
+        free(write_buf);
+        printf("write done, to fd %d(%s), total %d, st_size %d\n", fd, fn, total_size, file_stat.st_size);
         fflush(stdout);
     }
-    sleep(4);
+    sleep(2);
     unsigned char *read_buf = malloc(BLOCK_SIZE);
     if ((strcmp(argv[2], "r") == 0) || (strcmp(argv[2], "rw") == 0)) {
-        if (strcmp(argv[2], "r") == 0) {
-            int size;
-            replay_size = 0;
-            while(scanf("%d", &size) != EOF) {
-                replay_log[replay_size++] = size;
-                if (size > BLOCK_SIZE) {
-                    printf("size %d too large\n", size);
-                    exit(-1);
-                }
-                total_size += size;
-            }
-        }
-        fd = open(argv[1], O_RDONLY, 0666);
+        fd = open(fn, O_RDONLY, 0666);
         struct stat file_stat;
         fstat(fd, &file_stat);
         printf("open st_size %d\n", file_stat.st_size);
@@ -87,5 +102,6 @@ int main(int argc, char **argv) {
         }
         close(fd);
     }
-    return 0;
+    unlink(fn);
+    return NULL;
 }
