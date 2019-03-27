7f1f6e055a9d00f819ce968dbc4ea5e2d97bd8bd
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-03-27 16:10:42 -0700

    Fix for if extcache is enabled

--- libfs/src/log/log.c
+++ libfs/src/log/log.c
@@ -1298,14 +1298,16 @@ void handle_digest_response(char *ack_cmd)
 #ifdef EXTCACHE
     // unset uptodate flag of all buffer heads
     // all buffer heads should point to extent tree nodes
-    struct block_device *rootdev = g_bdev[g_root_dev];
-    pthread_mutex_lock(&rootdev->bd_bh_root_lock);
-    struct rb_root *root = &rootdev->bd_bh_root;
-    struct buffer_head *bh, *_bh;
-    rbtree_postorder_for_each_entry_safe(bh, _bh, root, b_rb_node) {
-    clear_buffer_uptodate(bh);
+    if (g_idx_choice == NONE) {
+        struct block_device *rootdev = g_bdev[g_root_dev];
+        pthread_mutex_lock(&rootdev->bd_bh_root_lock);
+        struct rb_root *root = &rootdev->bd_bh_root;
+        struct buffer_head *bh, *_bh;
+        rbtree_postorder_for_each_entry_safe(bh, _bh, root, b_rb_node) {
+            clear_buffer_uptodate(bh);
+        }
+        pthread_mutex_unlock(&rootdev->bd_bh_root_lock);
     }
-    pthread_mutex_unlock(&rootdev->bd_bh_root_lock);
 #endif
 	// persist log superblock.
 	write_log_superblock(g_log_sb);
