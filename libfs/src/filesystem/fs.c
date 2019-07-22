e7b7214b2a80fa312298d32a8791d65f4f4b524a
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-07-22 08:52:11 -0700

    Add integration with metadata caching.
    - Removed extra copy overhead.

--- libfs/src/filesystem/fs.c
+++ libfs/src/filesystem/fs.c
@@ -559,10 +559,10 @@ int sync_inode_ext_tree(uint8_t dev, struct inode *inode)
         int api_err = mlfs_hash_cache_invalidate();
         if (api_err) return api_err;
     } else {
-        if (IDXAPI_IS_PER_FILE()) {
-            FN(inode->ext_idx, im_clear_metadata_cache, inode->ext_idx);
+        if (IDXAPI_IS_PER_FILE() && inode->ext_idx) {
+            FN(inode->ext_idx, im_clear_metadata, inode->ext_idx);
             
-            if (g_idx_cached && inode->ext_idx) {
+            if (g_idx_cached) {
                 int api_err = FN(inode->ext_idx, im_invalidate, inode->ext_idx);
                 if (api_err) return api_err;
             }
